7cf5846f50aef78ce604fa6bcf537bff8d24f9c6
Doğuş Yeşilkaya <dcyesilkaya@hotmail.com>
2018-06-22 15:10:22 +0300

    SimpleTwitter

---
+++ SimpleTwitter/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleTwitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //index html çağrıldığı zaman kök dizindeki(wwwroot) un altındaki index htmlini çalıştır dedik
+            return File("~/index.html", "text/html");
+        }
+    }
+}
---
+++ SimpleTwitter/Controllers/TweetsController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SimpleTwitter.Data;
+using SimpleTwitter.Models;
+
+namespace SimpleTwitter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TweetsController : ControllerBase
+    {
+        private readonly ApplicationDbContext db;
+        public TweetsController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        public IEnumerable<Tweet> Get()
+        {
+            return db.Tweets;
+        }
+        // GET api/values
+        [HttpGet("{id}")]
+        public Tweet Get(int id)
+        {
+            return db.Tweets.FirstOrDefault(t => t.Id == id);
+        }
+        [HttpPost]
+        public void Post([FromBody] Tweet tweet)
+        {
+            
+            tweet.Date = DateTime.Now;
+            db.Tweets.Add(tweet);
+            db.SaveChanges();
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Tweet tweet)
+        {
+            var tw = db.Tweets.FirstOrDefault(t => t.Id == id);
+            tw.Text = tweet.Text;
+            tw.Date = DateTime.Now;
+            db.SaveChanges();
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var tweet = db.Tweets.FirstOrDefault(t => t.Id == id);
+            db.Tweets.Remove(tweet);
+            db.SaveChanges();
+        }
+    }
+}
